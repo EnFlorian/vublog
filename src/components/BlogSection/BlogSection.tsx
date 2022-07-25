f92b42d9439cc3819c8dc832fec7a6a578ff8c89
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-25 17:52:07 +0200

    Feat: Added main section for posts

--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -1,7 +1,17 @@
 import "./BlogSection.scss";
+import { posts } from "../../data";
+import PostCardLarge from "../PostCardLarge/PostCardLarge";
 
 const BlogSection = () => {
-  return <div>BlogSection</div>;
+  const cards = posts.map((post, idx) => {
+    return <PostCardLarge key={idx} {...post} />;
+  });
+
+  return (
+    <section className="blog-section">
+      <div className="blog-section__cards-container">{cards}</div>
+    </section>
+  );
 };
 
 export default BlogSection;
