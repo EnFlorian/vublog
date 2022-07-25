7101089e120bd9d1d39ab06cac5021d8e3ebfc4a
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-25 17:46:13 +0200

    Feat: Added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">&copy; {new Date().getFullYear()} - VuBlog</p>
+    </footer>
+  );
 };
 
 export default Footer;
