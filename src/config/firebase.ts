a72a22e9d8c19f5b54bbd1db8440a3ba155370f3
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-25 10:40:44 +0200

    Chore: Added Firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,1 +1,13 @@
-export {}
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {};
+const app = initializeApp(firebaseConfig);
+
+const fireDb = getFirestore(app);
+const fireStore = getStorage(app);
+const fireAuth = getAuth(app);
+
+export { fireDb, fireStore, fireAuth };
