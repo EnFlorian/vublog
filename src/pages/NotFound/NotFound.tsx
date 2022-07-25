5a7c4987a63b0b0fbeb6a682e790b3c3544fa39e
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-25 17:49:22 +0200

    Feat: Added 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,11 @@
 import "./NotFound.scss";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__description">Page was not found.</p>
+    </section>
+  );
 };
-
 export default NotFound;
