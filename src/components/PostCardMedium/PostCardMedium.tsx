17c4924dee8345f9f2b6e570cc771e45774cbfb7
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:24:40 +0200

    Feat: Added medium sized post card

--- src/components/PostCardMedium/PostCardMedium.tsx
+++ src/components/PostCardMedium/PostCardMedium.tsx
@@ -1,7 +1,24 @@
 import "./PostCardMedium.scss";
+import { Link } from "react-router-dom";
+import { IPost } from "../../state/types";
 
-const PostCardMedium = () => {
-  return <div>PostCardMedium</div>;
+const PostCardMedium = ({ title, id, summary, image, author, createdAt }: IPost) => {
+  return (
+    <section className="post-card-medium">
+      <img src={image} alt="" className="post-card-medium__image" />
+      <Link to={`/post/${id}`}>
+        <h1 className="post-card-medium__title">{title}</h1>
+      </Link>
+      <p className="post-card-medium__text">{summary}</p>
+      <section className="post-card-medium__details">
+        <img src={author.image} alt="Author" className="post-card-medium__author-image" />
+        <section className="post-card-medium__info-container">
+          <h1 className="post-card-medium__author-name">{author.name}</h1>
+          <p className="post-card-medium__info">{createdAt} </p>
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default PostCardMedium;
