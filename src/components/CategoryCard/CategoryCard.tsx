438d5a55817658c0b3e74f34dd68b707946e6ad3
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:28:53 +0200

    Feat: Added card for the catagories

--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,7 +1,21 @@
 import "./CategoryCard.scss";
 
-const CategoryCard = () => {
-  return <div>CategoryCard</div>;
+interface ICategoryCardProps {
+  name: string;
+  icon: JSX.Element;
+}
+
+const CategoryCard = ({ name, icon }: ICategoryCardProps) => {
+  return (
+    <div className="category-card">
+      <div className="category-card__left-content">
+        <i className="category-card__icon">{icon} </i>
+      </div>
+      <div className="category-card__right-content">
+        <p className="category-card__name">{name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryCard;
