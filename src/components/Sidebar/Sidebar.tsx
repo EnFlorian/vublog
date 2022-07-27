edcca4f8f6280b20f0f44a239e61c19d703e2ae8
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:25:02 +0200

    Feat: Added sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,40 @@
 import "./Sidebar.scss";
+import SectionDivider from "../SectionDivider/SectionDevider";
+import { categories } from "../../data";
+import CategoryCard from "../CategoryCard/CategoryCard";
+import Socials from "../Socials/Socials";
+import NewsLetter from "../NewsLetter/NewsLetter";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const categorieCards = categories.map((category) => {
+    return (
+      <li key={category.id}>
+        <Link to={`/posts/${category.name}`}>
+          <CategoryCard {...category} />
+        </Link>
+      </li>
+    );
+  });
+
+  return (
+    <section className="sidebar">
+      <section className="sidebar__section">
+        <SectionDivider title="Categories" />
+        <ul className="sidebar__categories-container">{categorieCards}</ul>
+      </section>
+      <section className="sidebar__section">
+        <SectionDivider title="Socials" />
+        <div className="sidebar__socials-container">
+          <Socials />
+        </div>
+      </section>
+      <section>
+        <SectionDivider title="Lets Stay in Touch" />
+        <NewsLetter />
+      </section>
+    </section>
+  );
 };
 
 export default Sidebar;
