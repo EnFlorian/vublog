eb3b64694272e78ba4131ef6ca9e7d565f767c2b
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:24:15 +0200

    Feat: Added the main blog card

--- src/components/PostCardLarge/PostCardLarge.tsx
+++ src/components/PostCardLarge/PostCardLarge.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 import { IPost } from "../../state/types";
+import { shortenText } from "../../utils";
 import "./PostCardLarge.scss";
 
-const PostCardLarge = ({ id, title, summary, author, createdAt, image }: IPost) => {
+const PostCardLarge = ({ id, title, summary, author, createdAt, image, category }: IPost) => {
   return (
     <section className="post-card-large">
       <div className="post-card-large-content">
         <img src={image} alt="" className="post-card-large__image" />
       </div>
       <section className="post-card-large__right-content">
+        <Link className="post-card-large__category" to={`/posts/${category}`}>
+          {category}
+        </Link>
         <Link to={`/post/${id}`}>
           <h1 className="post-card-large__title">{title}</h1>
         </Link>
-        <p className="post-card-large__text">{summary}</p>
+        <p className="post-card-large__text">{shortenText(summary, 155)}</p>
         <section className="post-card-large__details">
           <img src={author.image} alt={author.name} className="post-card-large__author-image" />
           <section className="post-card-large__info-container">
