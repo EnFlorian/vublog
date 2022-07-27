6e8678846885d2182c003c7ec4ad16b626172008
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:32:31 +0200

    Feat: Added social component

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,7 +1,23 @@
 import "./Socials.scss";
+import { socials } from "../../data";
+import { Link } from "react-router-dom";
 
 const Socials = () => {
-  return <div>Socials</div>;
+  const socialCards = socials.map((social, idx) => {
+    return (
+      <li key={idx}>
+        <Link to={social.path}>
+          <i className="socials__icon">{social.icon}</i>
+        </Link>
+      </li>
+    );
+  });
+
+  return (
+    <div className="socials">
+      <ul className="socials__container">{socialCards}</ul>
+    </div>
+  );
 };
 
 export default Socials;
