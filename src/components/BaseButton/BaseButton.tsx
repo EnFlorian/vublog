427a2a7a5747ba4879c3aa269377878389f997a7
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:22:15 +0200

    Fix: Fixed event handling

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -3,12 +3,23 @@ import "./BaseButton.scss";
 interface IBaseButtonProps {
   variant: "primary" | "secondary";
   name: string;
-  children: React.ReactNode;
-  onClick?: () => void;
+  children?: React.ReactNode;
+  func?: () => void;
 }
 
-const BaseButton = ({ variant, name }: IBaseButtonProps) => {
-  return <button className={`base-button base-button--${variant}`}>{name}</button>;
+const BaseButton = ({ variant, name, func }: IBaseButtonProps) => {
+  const handleClick = (e: React.MouseEvent) => {
+    if (func) {
+      e.preventDefault();
+      func && func();
+    }
+  };
+
+  return (
+    <button aria-label="button" className={`base-button base-button--${variant}`} onClick={(e) => handleClick(e)}>
+      {name}
+    </button>
+  );
 };
 
 export default BaseButton;
