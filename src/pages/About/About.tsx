210ba2cd72e6ae3c3fa2d575061102d2685cf886
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:28:25 +0200

    Feat: Added about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,40 @@
-import "./About.scss"
+import "./About.scss";
+import { about } from "../../data";
+import { SectionDivider } from "../../components";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="about">
+      <div className="about__wrapper container">
+        <section className="about__header">
+          <h1 className="about__title">About Me</h1>
+          <p className="about__description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur
+            consectetur.
+          </p>
+          <div className="about__image-wrapper">
+            <img className="about__image" src={about.image} alt="about" />
+          </div>
+        </section>
+        <section className="about__content">
+          <div className="about__left-content">
+            <SectionDivider title="Who am I?" />
+            <p className="about__text">{about.background}</p>
+          </div>
+          <div className="about__right-content">
+            <section className="about__section">
+              <SectionDivider title="Web Development" />
+              <p className="about__text">{about.webdev}</p>
+            </section>
+            <section className="about__section">
+              <SectionDivider title="DevOps" />
+              <p className="about__text">{about.devops}</p>
+            </section>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
