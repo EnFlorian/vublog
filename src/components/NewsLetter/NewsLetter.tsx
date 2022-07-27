3964c8a1fd0a929f5167bec44a87c2de73522230
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-27 15:30:06 +0200

    Feat: Added newletter subscription component

--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { useUIStore } from "../../state/store";
+import BaseButton from "../BaseButton/BaseButton";
 import "./NewsLetter.scss";
 
 const NewsLetter = () => {
-  return <div>NewsLetter</div>;
+  const { openNotificationModal } = useUIStore((state) => state);
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    openNotificationModal();
+  };
+
+  return (
+    <section className="newsletter">
+      <div className="newsletter__wrapper">
+        <p className="newsletter__text"> Subscribe and get the latest news and updates.</p>
+        <form className="newsletter__form">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="newsletter__input"
+            type="email"
+            placeholder="Enter Email..."
+          />
+          <BaseButton func={openNotificationModal} variant="primary" name="Subscribe" />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default NewsLetter;
